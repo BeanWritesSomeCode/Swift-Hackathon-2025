fba6de197462cf66b1ae237bce79602bfff07299
BeanWritesSomeCode <johnsonbd7@vcu.edu>
2025-07-17 13:32:22 -0400

    Wrapped current pages in DOM Router

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,14 +3,19 @@ import Header from "./components/header"
 import MainContainer from './components/Main-Container'
 import './styles.css'
 import './App.css'
+import { Routes, Router, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header/>
-    <MainContainer/>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<MainContainer />}/>
+          //Pages go here!
+        </Route>
+      </Routes>
     </>
   )
 }
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,20 +1,24 @@
 import sparkLogo from '../assets/sparkDarker.png'
 import avatar from '../assets/avatar.jpg'
+import { Outlet } from 'react-router-dom';
 
 export function Header() {
     return(
+        <>
     <header className="header-bar">
         <img src={sparkLogo} alt="Spark Logo" className="sparkLogo"/>
         <h1 className="logo"><a href="homepage.html" className="no-underline">SPARK</a></h1>
         <nav>
-            <a href="#">Home</a>
-            <a href="#">Community</a>
-            <a href="#">Knowledge Zone</a>
-            <a href="#">Personalized Updates</a>
+            <a href="/">Home</a>
+            <a href="/community">Community</a>
+            <a href="/knowledge">Knowledge Zone</a>
+            <a href="/personal">Personalized Updates</a>
         </nav>
          <img src={avatar} alt="Avatar" className="avatar"/>
          Tomahito Mori
     </header>
+    <Outlet />
+    </>
 )
 }
 
