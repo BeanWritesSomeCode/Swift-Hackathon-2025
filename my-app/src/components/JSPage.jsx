58559f1887c63932d98b961c28255127f2bb0781
BeanWritesSomeCode <johnsonbd7@vcu.edu>
2025-07-17 19:15:01 -0400

    Did some stuff

--- my-app/src/components/JSPage.jsx
+++ my-app/src/components/JSPage.jsx
@@ -1,16 +1,16 @@
 import MainContainer from './Main-Container'
 import App from "@/App.jsx";
 import '../css/jspage.css'
+import thing from "@/assets/sparkDarker.png"
 function JSPage() {
     return (
         <>
             <div className="forum-container">
-                <ul>
-                    <li><div><p>1</p></div></li>
-                    <li><div><p>1</p></div></li>
-                    <li><div><p>1</p></div></li>
-                    <li><div><p>1</p></div></li>
-                </ul>
+                <div className='forum-row'><img alt='img' src={thing}></img><p>How 2 React</p></div>
+                <div className='forum-row'>2</div>
+                <div className='forum-row'>3</div>
+                <div className='forum-row'>4</div>
+                <div className='forum-row'>5</div>
             </div>
         </>
     )
