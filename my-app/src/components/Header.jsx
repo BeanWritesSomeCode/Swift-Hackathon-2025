a24aac678e36066b1f2c2b2263c5862a17234e75
Tomahito Mori <mori846@lynchburg.edu>
2025-07-17 12:54:58 -0400

    Added some stuff

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import sparkLogo from '../assets/sparkDarker.png'
+import avatar from '../assets/avatar.jpg'
+
+export function Header() {
+    return(
+    <header className="header-bar">
+        <img src={sparkLogo} alt="Spark Logo" className="sparkLogo"/>
+        <h1 className="logo"><a href="homepage.html" className="no-underline">SPARK</a></h1>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Community</a>
+            <a href="#">Knowledge Zone</a>
+            <a href="#">Personalized Updates</a>
+        </nav>
+         <img src={avatar} alt="Avatar" className="avatar"/>
+    </header>
+)
+}
+
+export default Header
+
